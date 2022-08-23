736b9beee27e9e4f53d058c6c2a668799018a1d3
johnboy <johnervine@hotmail.com>
2022-08-23 17:35:11 +0100

    Implement Edit & Delete Posts

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -26,6 +26,35 @@ class ListingController extends Controller
         return view('listings.create');
     }
 
+    public function edit(Listing $listing) {
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+    public function delete(Listing $listing) {
+        $listing->delete();
+        return redirect('/')->with('message', 'Listing Deleted Successfully');
+    }
+
+    public function update(Request $request, Listing $listing) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => 'required',
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('message', 'Listing Updated Successfully');
+    }
+
     public function store(Request $request) {
         $formFields = $request->validate([
             'title' => 'required',
