ad3cea60ffa3ceb850649838ab22b001450dfc73
johnboy <johnervine@hotmail.com>
2022-08-23 16:29:06 +0100

    Implement Pagination

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -14,5 +14,8 @@
     @foreach($listings as $listing)
         <x-listing-card :listing="$listing"/>
     @endforeach
-    @endsection
 </div>
+<div class="mt-6 p-4">
+    {{$listings->links()}}
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ use App\Http\Controllers\ListingController;
 */
 
 Route::get('/', [ListingController::class, 'index']);
+Route::get('/listings/create', [ListingController::class, 'create']);
+Route::post('/listings', [ListingController::class, 'store']);
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
